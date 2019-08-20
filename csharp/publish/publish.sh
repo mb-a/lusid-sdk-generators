e9bbf0b40ead98d4e701b23827b01fba09c3d1fd
Mike McGarry <mikemcgarry@live.com.au>
2019-08-20 13:09:01 +0100

    Updated the Csharp Dockerfile and publish files

--- csharp/publish/publish.sh
+++ csharp/publish/publish.sh
@@ -1,25 +1,24 @@
-#!/bin/bash 
-
-set -e
+#!/bin/bash -e
 
 if [[ (${#1} -eq 0) ]] ; then
     echo 
     echo "[ERROR] missing API key"
-    echo
     exit 1
 fi
 
 if [[ (${#2} -eq 0) ]] ; then
     echo 
-    echo "[ERROR] missing NuGet repo"
-    echo
+    echo "[ERROR] missing repo URL"
     exit 1
 fi
 
-nuget_api_key=$1
-nuget_source=$2
+api_key=$1
+repo_url=$2
 
 sdk_version=$(cat lusid.json | jq -r '.info.version')
+
+# check the SDK builds
+dotnet build sdk/Lusid.Sdk.sln
 
 sed -i 's/<Version>.*<\/Version>/<Version>'$sdk_version'<\/Version>/g' sdk/Lusid.Sdk/Lusid.Sdk.csproj
 
@@ -27,5 +26,5 @@ echo "sdk_version=$sdk_version"
 
 dotnet pack -c Release sdk/Lusid.Sdk/Lusid.Sdk.csproj
 dotnet nuget push sdk/Lusid.Sdk/bin/Release/Lusid.Sdk.${sdk_version}.nupkg \
-    --source $nuget_source \
-    --api-key $nuget_api_key
+    --source $repo_url \
+    --api-key $api_key
