f27744deffe0318375775b9164ab1b32459da214
Rizwan Saeed <rizwan.saeed@finbourne.com>
2020-02-17 16:56:19 +0000

    DEV-6784 Parameterise C# generation

--- csharp/generate/generate.sh
+++ csharp/generate/generate.sh
@@ -21,18 +21,21 @@ fi
 gen_root=$1
 output_folder=$2
 swagger_file=$output_folder/$3
-
+sdk_config=$4
 sdk_output_folder=$output_folder/sdk
 
 ignore_file_name=.openapi-generator-ignore
-config_file_name=config.json
+config_file_name="${sdk_config:=config.json}"
+
 config_file=$gen_root/$config_file_name
 ignore_file=$output_folder/$ignore_file_name
+
+app_name=$(cat $config_file | jq -r .packageName)
 
 #   remove all previously generated files
 shopt -s extglob 
 echo "removing previous sdk:"
-rm -rf $sdk_output_folder/Lusid.Sdk/!(Utilities|*.csproj)
+rm -rf $sdk_output_folder/$app_name/!(Utilities|*.csproj)
 shopt -u extglob 
 
 # ignore files
@@ -45,7 +48,7 @@ sdk_version=$(cat $swagger_file | jq -r '.info.version')
 echo "setting version to $sdk_version"
 
 cat $config_file | jq -r --arg SDK_VERSION "$sdk_version" '.packageVersion |= $SDK_VERSION' > temp && mv temp $config_file
-sed -i 's/<Version>.*<\/Version>/<Version>'$sdk_version'<\/Version>/g' $sdk_output_folder/Lusid.Sdk/Lusid.Sdk.csproj
+sed -i 's/<Version>.*<\/Version>/<Version>'$sdk_version'<\/Version>/g' $sdk_output_folder/$app_name/$app_name.csproj
 
 echo "generating sdk"
 
